c37490cf9281f39e9423baab78998cac6861c19d
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-01-05 13:46:43 -0300

    feat: :sparkles: add negociacao controller

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,10 @@
-import { Negociacao } from './models/negociacao.js';
+import { NegociacaoController } from './controller/negociacao-contoller.js';
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-console.log(negociacao.data);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  controller.adiciona();
+});
