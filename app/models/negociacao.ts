b97eaf87b15a1e2a769cd6e82bdf51fb9de7bca1
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-01-05 12:34:48 -0300

    feat: :safety_vest: automatic update server

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,26 +1,26 @@
 export class Negociacao {
-    #data;
-    #quantidade;
-    #valor
+  #data;
+  #quantidade;
+  #valor;
 
-    constructor(data, quantidade, valor){
-      this.#data = data;
-      this.#quantidade = quantidade;
-      this.#valor = valor;
-    }
-    
-    get data() {
-      return this.#data;
-    }
+  constructor(data, quantidade, valor) {
+    this.#data = data;
+    this.#quantidade = quantidade;
+    this.#valor = valor;
+  }
 
-    get quantidade(){
-      return this.#quantidade;
-    }
+  get data() {
+    return this.#data;
+  }
 
-    get valor(){
-      return this.#valor;
-    }
-    get volume() {
-      return this.#quantidade * this.#valor;
-    }
+  get quantidade() {
+    return this.#quantidade;
+  }
+
+  get valor() {
+    return this.#valor;
+  }
+  get volume() {
+    return this.#quantidade * this.#valor;
+  }
 }
