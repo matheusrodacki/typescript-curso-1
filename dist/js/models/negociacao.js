5d1673b044998dfd260e4f0e749da185a2cdd195
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-01-05 13:22:47 -0300

    fix: :pencil2: change to typescript sintax

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,22 +1,19 @@
 export class Negociacao {
     constructor(data, quantidade, valor) {
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
-    #data;
-    #quantidade;
-    #valor;
     get data() {
-        return this.#data;
+        return this._data;
     }
     get quantidade() {
-        return this.#quantidade;
+        return this._quantidade;
     }
     get valor() {
-        return this.#valor;
+        return this._valor;
     }
     get volume() {
-        return this.#quantidade * this.#valor;
+        return this._quantidade * this._valor;
     }
 }
