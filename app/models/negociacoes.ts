a942dc1f9029744f39b0d107f903447917b4bf88
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-01-07 03:43:45 -0300

    feat: :sparkles: add: lista de negociações

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,12 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+  private negociacoes: Array<Negociacao> = [];
+
+  adiciona(negociacao: Negociacao): void {
+    this.negociacoes.push(negociacao);
+  }
+  lista(): Array<Negociacao> {
+    return this.negociacoes;
+  }
+}
