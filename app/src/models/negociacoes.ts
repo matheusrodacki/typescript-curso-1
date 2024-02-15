9123bf3df1d0d289636b8fd4bf44a18d3087c3d9
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-02-15 06:08:56 -0300

    implementa interface Comparavel

--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,7 +1,8 @@
+import { Comparavel } from '../interfaces/comparavel.js';
 import { Imprimivel } from '../services/imprimivel.js';
 import { Negociacao } from './negociacao.js';
 
-export class Negociacoes implements Imprimivel {
+export class Negociacoes implements Imprimivel, Comparavel<Negociacoes> {
   private negociacoes: Array<Negociacao> = [];
 
   public adiciona(negociacao: Negociacao): void {
@@ -12,5 +13,10 @@ export class Negociacoes implements Imprimivel {
   }
   public paraTexto(): string {
     return JSON.stringify(this.negociacoes, null, 2);
+  }
+  public ehIgual(negociacoes: Negociacoes): boolean {
+    return (
+      JSON.stringify(this.negociacoes) === JSON.stringify(negociacoes.lista())
+    );
   }
 }
